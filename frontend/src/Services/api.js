79e51dfb37d7a335e6a58470282bc72cd5e60a91
NamrataDalvi01 <dalvisnamrata07@gmail.com>
2026-07-28 22:32:04 +0530

    Connect frontend with Render backend

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://stockapp-n.onrender.com",
 });
 
 export const getStock = async (symbol) => {
